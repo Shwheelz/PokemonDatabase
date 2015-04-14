538e573cfc7d856f32d58e56d25f9f581ca53f23
Shwheelz <sfbesong@gmail.com>
2015-04-13 21:43:22 -0400

    added bootstrap

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,6 @@
 class PokemonsController < ApplicationController
   before_action :set_pokemon, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, except: [:index, :show]
   # GET /pokemons
   # GET /pokemons.json
   def index
