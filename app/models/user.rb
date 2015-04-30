77ec4bf4a62370b075942fdb27eba975f97f6325
Shwheelz <sfbesong@gmail.com>
2015-04-29 20:30:14 -0400

    added dependent to foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :pokemons
+  has_many :pokemons, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
